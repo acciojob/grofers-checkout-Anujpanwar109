bcd5fa7db2d69070258adaa1305ddb7cdfca282a
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-14 11:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let table=document.querySelector("table");
+	let prices=document.querySelectorAll(".price");
+	let lastrow=document.createElement('tr')
+	table.appendChild(lastrow);
+	let cell1=lastrow.insertCell(0);
+	let cell2=lastrow.insertCell(1);
+	cell1.textContent="total";
+
 const getSum = () => {
 //Add your code here
+	
+	let total=0;
+	prices.forEach(price => {
+  total += parseFloat(price.textContent) || 0; 
+});
+	cell2.textContent = total.toFixed(2);
   
 };
 
